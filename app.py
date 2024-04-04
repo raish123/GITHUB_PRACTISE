609eca859e2b8fc3ea03f95fbd9f6319abb8c961
Raees Azam Shaikh <111172525+raish123@users.noreply.github.com>
2024-04-04 16:54:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ def add(a,b):
 
 #another branch_root(raees_developer) will develop newline_code into app.py file 
 
-def subtract(a,b):
-    return a-b
+def subtract(a,b,d):
+    return a-b-d
 
 #now raees_developer want to merge the line of code into main branch
 
