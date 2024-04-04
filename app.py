2a20f09de22fb1662fe859176cefb9ccaa94d9f9
Raees Azam Shaikh <shaikhraishazam@gmail.com>
2024-04-04 16:26:30 +0530

    raees_developer code present in app.py

--- app.py
+++ app.py
@@ -2,3 +2,10 @@
 def add(a,b):
     return a+b
 
+
+#another branch_root(raees_developer) will develop newline_code into app.py file 
+
+def subtract(a,b):
+    return a-b
+
+#now raees_developer want to merge the line of code into main branch
